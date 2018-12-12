3745b315d8cd94596dc9c4003c4f08c076e82ed3
harikishore.maridi <harikishore.maridi@sciits.com>
2018-12-12 14:18:54 +0530

    swagger config jwt

---
+++ spring-boot-jwt-swagger/src/main/java/io/jwt/main/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package io.jwt.main.repository;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.jwt.main.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+  boolean existsByUsername(String username);
+
+  User findByUsername(String username);
+
+  @Transactional
+  void deleteByUsername(String username);
+
+}
